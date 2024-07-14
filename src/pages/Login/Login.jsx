5b9d492a9958236a900267e01c42778e6bd6531b
Dheeraj Atmakuri <dheerajatmakuri10010@gmail.com>
2024-07-15 00:55:03 +0530

    modified:   package-lock.json modified:   package.json modified:   src/App.jsx modified:   src/components/Navbar/Navbar.jsx new file:   src/firebase.js modified:   src/pages/Login/Login.jsx

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,23 +1,56 @@
 import React, { useState } from 'react'
 import './Login.css'
 import logo from '../../assets/logo.png';
+import { login, signup } from '../../firebase';
 
 const Login = () => {
 
-  const [signState, setSignState] = useState("Sign In")
-  
+  const [signState, setSignState] = useState("Sign In");
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  // const user_auth = async (event) => {
+  //   event.preventDefault();
+  //   try {
+  //     if (signState === "Sign In") {
+  //       await login(email, password);
+  //     } else {
+  //       await signup(name, email, password);
+  //     }
+  //   } catch (error) {
+  //     if (error.code === 'auth/invalid-credential') {
+  //       alert('The password you entered is incorrect. Please try again.');
+  //     } else if (error.code === 'auth/email-already-in-use') {
+  //       alert('This email is already in use. Please use a different email or sign in.');
+  //     } else {
+  //       alert(error.message);
+  //     }
+  //   }
+  // }
+
+  const user_auth = async (event) => {
+    event.preventDefault();
+    if(signState==="Sign In"){
+      await login(email, password);
+    }else{
+      await signup(name, email, password);
+    }
+  }
+
   return (
     <div className='login'>
       <img src={logo} className='login-logo' alt="" />
       <div className="login-form">
         <h1>{signState}</h1>
         <form>
-          {signState==="Sign Up"?<input type="text" placeholder='Your Name' />:<></>}
+          {signState==="Sign Up"?
+          <input value={name} onChange={(e) => {setName(e.target.value)}} type="text" placeholder='Your Name' />:<></>}
           
-          <input type="email" placeholder='Email ID' />
-          <input type="password" placeholder='Password' />
-        {/* <h1>{signState}</h1> */}
-          <button>{signState}</button>
+          <input value={email} onChange={(e)=>{setEmail(e.target.value)}} type="email" placeholder='Email Id' />
+
+          <input value={password} onChange={(e)=>{setPassword(e.target.value)}} type="password" placeholder='Password' />
+          <button onClick={user_auth} type='submit'>{signState}</button>
           <div className="form-help">
             <div className="remember">
               <input type="checkbox" />
